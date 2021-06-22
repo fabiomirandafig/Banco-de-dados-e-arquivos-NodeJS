3ae42b1868ed01f636710a0daf3f33622cf54da1
Fábio <fabiomirandafig@gmail.com>
2021-06-21 22:05:33 -0300

    Versão final

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,5 +5,7 @@ import transactionsRouter from './transactions.routes';
 const routes = Router();
 
 routes.use('/transactions', transactionsRouter);
+routes.use('/transactions/:id', transactionsRouter);
+routes.use('/transactions/import', transactionsRouter);
 
 export default routes;
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,26 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import Transaction from '../models/Transaction';
+import AppError from '../errors/AppError';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<AppError> {
+    const deleteTransactionRepository = getRepository(Transaction);
+
+    const findId = await deleteTransactionRepository.findOne({
+      where: { id },
+    });
+
+    if (!findId) {
+      throw new AppError('Transação não existente!');
+    }
+
+    await deleteTransactionRepository.delete(id);
+
+    return new AppError('Deletada com sucesso!', 204);
   }
 }
 
